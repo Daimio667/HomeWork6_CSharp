3e7c47088863647e927f634ced25f3e9f69f15e7
Oleg <daimio667@gmail.com>
2022-09-20 22:59:55 +0300

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Task 41 : Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+// больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.WriteLine("Enter numbers separated by commas : ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.Write($"Entered {count} numbers greater than zero");
